c56f601242b9dd6a6645b4927d1c015cc99b08db
Aaron Turner <torch2424@hotmail.com>
2015-05-07 13:09:50 -0700

    Got Player moving more than once, and Discovered a way to make maps and things

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Player : MonoBehaviour 
+{
+	public Rigidbody player;
+	public float moveSpeed = 0f;
 	
+	void Update ()
+	{
+		Move();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Move ()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical"); 
+		Vector3 movement = new Vector3(h, v, 0f); 
+		player.AddForce(movement * moveSpeed);
 	}
 }
