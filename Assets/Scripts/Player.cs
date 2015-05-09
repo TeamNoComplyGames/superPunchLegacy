43c3020f547745dc7a8c1e29adc4d752c24b4d0c
Aaron Turner <torch2424@hotmail.com>
2015-05-09 10:37:23 -0700

    Got player moving alot smoother, things looking nice

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Player : MonoBehaviour 
 {
-	public Rigidbody player;
+	public Rigidbody2D player;
 	public float moveSpeed = 0f;
 	
 	void Update ()
@@ -15,7 +15,7 @@ public class Player : MonoBehaviour
 	{
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical"); 
-		Vector3 movement = new Vector3(h, v, 0f); 
-		player.AddForce(movement * moveSpeed);
+		Vector2 movement = new Vector3(h, v); 
+		player.MovePosition(player.position + movement * moveSpeed);
 	}
 }
