89f149346e068e0fa61e73ce224c38299cab3831
Aaron Turner <torch2424@hotmail.com>
2015-05-16 22:14:11 -0700

    Wrote out the pause menu script and created our pause menu in the inspector

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour 
+{
+	//Our UI
+	public GameObject pauseMenu;
+
+	//our boolean so if we are paused
+	private bool paused;
+
+	void Start()
+	{
+		//initialize our variables
+		pauseMenu.SetActive (false);
+		paused = false;
+	}
+
+	void Update()
+	{
+		//If we are not paused
+		if (!paused) {
+			//Get our key input
+			if (Input.GetKey (KeyCode.KeypadEnter) || Input.GetKey ("return") || Input.GetKey ("escape")) {
+				//Pause the game
+				paused = true;
+				Time.timeScale = 0;
+			}
+		} 
+		//if we are paused
+		else 
+		{
+			if (Input.GetKey (KeyCode.KeypadEnter) || Input.GetKey ("return")) 
+			{
+				//resume the game
+				paused = false;
+				Time.timeScale = 1;
+			}
+			else if (Input.GetKey("escape"))
+			{
+				//Quit the game
+				Application.Quit();
+			}
+		}
+	}
+}
