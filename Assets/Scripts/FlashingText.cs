d77cdb9bcd1838d16583a7658470366627b00ea7
Aaron Turner <torch2424@hotmail.com>
2015-05-18 20:37:04 -0700

    Added some flashing text to our title screen

---
+++ Assets/Scripts/FlashingText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashingText : MonoBehaviour 
+{
+	//Our text
+	public UnityEngine.UI.Text text;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Call our flash function multiple times
+		InvokeRepeating("flash", 0 , 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	//Our flahing text function
+	private void flash()
+	{
+		//If the text is eneabled disable it, else renable
+		if (text.enabled) {
+			text.enabled = false;
+		} else {
+			text.enabled = true;
+		}
+	}
+}
