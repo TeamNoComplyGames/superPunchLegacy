a70c3716c6b51eda21d5502f054c893db611f7e9
Aaron Turner <torch2424@hotmail.com>
2015-05-25 17:00:36 -0700

    Windowed lowered and now attacking enemies makes more sense range and feel wise, maybe expanded in the future

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -203,12 +203,14 @@ public class Player : MonoBehaviour
 				float playerY = gameObject.transform.position.y;
 				float enemyX = collision.gameObject.transform.position.x;
 				float enemyY = collision.gameObject.transform.position.y;
+				//Our window for our punch range
+				float window = .1f;
 
 				//Deal damage if we are facing the right direction, and they are not too above or around us
-				if((dir == 1 && enemyX >= playerX && enemyY <= (playerY + .05f) && enemyY >= (playerY - .05f)) ||
-				   (dir == 3 && enemyX <= playerX && enemyY <= (playerY + .05f) && enemyY >= (playerY - .05f)) ||
-				   (dir == 2 && enemyY >= playerY && enemyX <= (playerX + .05f) && enemyX >= (playerX - .05f)) ||
-				   (dir == 0 && enemyY <= playerY & enemyX <= (playerX + .05f) && enemyX >= (playerX - .05f)))
+				if((dir == 1 && enemyX >= playerX && enemyY <= (playerY + window) && enemyY >= (playerY - window)) ||
+				   (dir == 3 && enemyX <= playerX && enemyY <= (playerY + window) && enemyY >= (playerY - window)) ||
+				   (dir == 2 && enemyY >= playerY && enemyX <= (playerX + window) && enemyX >= (playerX - window)) ||
+				   (dir == 0 && enemyY <= playerY & enemyX <= (playerX + window) && enemyX >= (playerX - window)))
 				{
 				//Get the enemy and decrease it's health
 				Enemy e = (Enemy) collision.gameObject.GetComponent("Enemy");
