f48e3b8739c43b5f2b53ad9aa9a833b526f22f65
Aaron Turner <torch2424@hotmail.com>
2015-05-27 19:59:10 -0700

    Finished writing our camera scaling script, now just need to attatch

--- Assets/Scripts/GameAspect.cs
+++ Assets/Scripts/GameAspect.cs
@@ -24,34 +24,41 @@ public class GameAspect : MonoBehaviour
 		if (aspectRatio >= 1.25) 
 		{
 			//Change camera.orthagraphic size and ui scale values here
-			Camera.main.orthographicSize = 
+			Camera.main.orthographicSize = 0.85f;
+			uiScale.scaleFactor = .75f;
 		}
 		//4:3
 		else if (aspectRatio >= 1.33) 
 		{
-
+			//Change camera.orthagraphic size and ui scale values here
+			Camera.main.orthographicSize = 0.8f;
+			uiScale.scaleFactor = .8f;
 		}
 		//3:2
 		else if (aspectRatio >= 1.5) 
 		{
-			
+			//Change camera.orthagraphic size and ui scale values here
+			Camera.main.orthographicSize = 0.725f;
+			uiScale.scaleFactor = .9f;
 		}
 		//16:10
 		else if (aspectRatio >= 1.6) 
 		{
-			
+			//Change camera.orthagraphic size and ui scale values here
+			Camera.main.orthographicSize = 0.665f;
+			uiScale.scaleFactor = .95f;
 		}
 		//16:9
 		else if (aspectRatio >= 1.77) 
 		{
-			
+			//Change camera.orthagraphic size and ui scale values here
+			Camera.main.orthographicSize = 0.6f;
+			uiScale.scaleFactor = 1.0f;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		//Show our aspect ratio
-		Debug.Log (Camera.main.aspect);
 	}
 }
