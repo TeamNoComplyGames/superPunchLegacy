29c839cc7d7cb25c0190e2fe7e6928eddddc90f4
Aaron Turner <torch2424@hotmail.com>
2015-05-31 12:28:15 -0700

    Started the script to make a game save

---
+++ Assets/Scripts/PlayerSave.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
+
+public class PlayerSave
+{
+	//Our highest level
+	int level;
+
+	//Our highest score
+	long score;
+
+	//Our constructor
+	public PlayerSave()
+	{
+		//set level and score to zero
+		level = 0;
+		score = 0;
+
+		//Load the save
+		loadSave();
+	}
+
+	public void loadSave()
+	{
+
+	}
+}
