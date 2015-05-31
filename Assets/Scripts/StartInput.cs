5551534901954395363c340f8c009f5332f939a9
Aaron Turner <torch2424@hotmail.com>
2015-05-31 12:58:39 -0700

    Start screen cn now fetch our save and display it correctly

--- Assets/Scripts/StartInput.cs
+++ Assets/Scripts/StartInput.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StartInput : MonoBehaviour {
-
+public class StartInput : MonoBehaviour 
+{
+	//Our text
+	public UnityEngine.UI.Text score;
 	// Use this for initialization
 	void Start () 
 	{
-	
+		//set up the highscore infor stuff
+		//now get our save file
+		SaveManager.loadSave();
+
+
+		//now set the text of the score, placed in inspector, \t for tab
+		//throw our values in a function to return a string with our desired zeroes
+		score.text = "\t" + System.String.Format("{0:000}", SaveManager.getSaveLevel()) + "\t\t\t" + "10000000000";
 	}
 	
 	// Update is called once per frame
